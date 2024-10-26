8b7901fad81ae3ea30ac31fb3008ed67975491cf
MykolaDekusar <ndekusar97@gmail.com>
2024-10-26 17:25:01 +0200

    Testing of if, else if, and else

--- JavaIf/src/Main.java
+++ JavaIf/src/Main.java
@@ -3,10 +3,16 @@ public class Main {
 
 	public static void main(String[] args) {
 	
-		int age = 12;
-		if(age >= 18) {
+		int age = 65;
+		
+		if(age >=65) {
+			System.out.println("Sei un pensionato");
+		}else if(age >=18) {
 			System.out.println("Sei maggiorenne");
+		}else {
+			System.out.println("Sei minorenne");
 		}
+		
 	}
 
 }
