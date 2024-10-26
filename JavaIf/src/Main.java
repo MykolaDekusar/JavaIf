9739d4263ddc0555240e0b3957da47ca99827e5f
MykolaDekusar <ndekusar97@gmail.com>
2024-10-26 17:26:08 +0200

    Added some comments for clarity

--- JavaIf/src/Main.java
+++ JavaIf/src/Main.java
@@ -4,11 +4,13 @@ public class Main {
 	public static void main(String[] args) {
 	
 		int age = 65;
-		
+		//verifico se lìeta è maggiore o uguale a 65
 		if(age >=65) {
 			System.out.println("Sei un pensionato");
+		//se fallilsco il primo if verifico se l'eta è maggiore o uguale a 18
 		}else if(age >=18) {
 			System.out.println("Sei maggiorenne");
+		//se fallisco entrambi gli if di sopra allora stampo Sei minorenne
 		}else {
 			System.out.println("Sei minorenne");
 		}
